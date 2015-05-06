3003a3a49447a20d9a7772bffffa99b6a95aa399
Collin Mullis <cmullis@saintjoe.edu>
2015-05-06 16:27:32 -0400

    Added Comments to explain testThree and testFour

--- Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
@@ -21,10 +21,14 @@ public class ColorAttrDriver {
 		System.out.println(testTwo.toString());
 		
 		// Number three
+		// The reason that testThree prints differently, is because it is a Color Attr, so it will take the conditions youre
+		// giving it, and use it in the way it knows how to as a Color Attr, taking previous constructors from Attr
 		testThree = new ColorAttr("ColorThree", newColor);
 		System.out.println(testThree.toString());
 	
 		// Number four
+		// The reason testFour prints differenlty from testThree is because testFour is a Attr, and not a color Attr, 
+		// so it doesn't have the data member that Attr needs to get the color. 
 		testFour = new Attr("ColorFour", "yellow");
 		System.out.println(testFour.toString());
 		
